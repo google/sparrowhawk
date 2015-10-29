5ffcb406ebf903ac940802c39a54565c6e03824b
Richard Sproat <rws@google.com>
2015-10-29 14:52:49 -0400

    Update to add correct versions with license headers.

--- src/include/sparrowhawk/sentence_boundary.h
+++ src/include/sparrowhawk/sentence_boundary.h
@@ -1,3 +1,16 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Copyright 2015 and onwards Google, Inc.
 // Simple interface for splitting text into sentences. Uses a regular expression
 // to define plausible end-of-sentence markers, and allows for a list of
 // exceptions --- e.g. abbreviations that end in periods that would not normally
